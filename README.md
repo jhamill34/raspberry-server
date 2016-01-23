88b1ac01b0d24435ff899b45e9a8896c6824fc72
Joshua Rasmussen <xlr8runner@gmail.com>
2016-01-23 01:22:25 -0800

    Added user interface and TODOS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## TODO ##
 open up socket.io to poll for the inputs on the GPIOS to update the UI on the browser
+
+ get a list of 'usable pins' from the server
---
+++ gpioroutes.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+var Actions = require('./actions');
+
+router.get('/', function(req, res){
+  res.status(200).send('hello world');
+});
+
+/**
+ * GET /flash/:id
+ * @param {number} id
+ * @param {number} dur
+ * @returns json response of GPIO status
+ */
+router.get('/flash/:id', function(req, res){
+    Actions.flash(req.params.id, req.query.dur);
+    res.status(200).json({
+	status: 'flashed',
+	pin: req.params.id,
+	durr: req.query.dur
+    });
+});
+
+/**
+ * GET /open/:id
+ * @param {number} id
+ * @returns json of the status
+ */
+router.get('/open/:id', function(req, res){
+   Actions.open(req.params.id).then(function(){
+	res.status(200).json({
+	    status: 'on',
+	    success: true,
+	    pin: req.params.id
+	});
+    }).catch(function(err){
+	res.status(500).json({
+	    success: false,
+	    message: err
+	});
+    });
+});
+
+/**
+ * GET /on/:id
+ * @param {number} id
+ * @returns json of the status
+ */
+router.get('/on/:id', function(req, res){
+   Actions.on(req.params.id).then(function(){
+	res.status(200).json({
+	    status: 'on',
+	    success: true,
+	    pin: req.params.id
+	});
+    }).catch(function(err){
+	res.status(500).json({
+	    success: false,
+	    message: err
+	});
+    });
+});
+
+/**
+ * GET /off/:id
+ * @param {number} id
+ * @returns json of the status
+ */
+router.get('/off/:id', function(req, res){
+   Actions.off(req.params.id).then(function(){
+	res.status(200).json({
+	    status: 'off',
+	    success: true,
+	    pin: req.params.id
+	});
+    }).catch(function(err){
+	res.status(500).json({
+	    success: false,
+	    message: err
+	});
+    });
+});
+
+/**
+ * GET /close/:id
+ * @param {number} id
+ * @returns json of the status
+ */
+router.get('/close/:id', function(req, res){
+    Actions.close(req.params.id).then(function(){
+	res.status(200).json({
+	    status: 'closed',
+	    success: true,
+	    pin: req.params.id
+	});
+    }).catch(function(err){
+	res.status(500).json({
+	    success: false,
+	    message: err
+	});
+    });
+});
+
+module.exports = router;
